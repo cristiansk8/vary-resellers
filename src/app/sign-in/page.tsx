48f3eece8066fe7c99d9b35e18845bb16b5d509d
cristiansk8 <cdpsk8@gmail.com>
2025-05-23 15:41:25 -0500

    ajustes callback

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -6,8 +6,6 @@ export default function SignIn() {
   return (
     <div className='flex justify-center items-center h-screen'>
       <SignInForm />
-      <TestSupabase />
-
     </div>
   )
 }
--- src/components/auth/providerButtons.tsx
+++ src/components/auth/providerButtons.tsx
@@ -4,18 +4,23 @@ import { Icons } from "../icons";
 import { Button } from "../ui/button";
 import { Provider } from "@supabase/supabase-js";
 
-const SUPABASE_REDIRECT = process.env.SUPABASE_CALLBACK_URL;
+const SUPABASE_REDIRECT = process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URL;
 
 export default function Providerbuttons() {
   const supabase = createClientComponentClient();
+
   const signInWithProvider = async (provider: Provider) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: SUPABASE_REDIRECT, // <-- aquí usas la variable
       },
     });
+    if (error) {
+      console.error("OAuth sign-in error:", error);
+    }
   };
+
   return (
     <div className="grid grid-cols-2 gap-6">
       <Button variant="outline" onClick={() => signInWithProvider("github")}>
